713f29c6ec117f1242e0338910e2af21d4701ddc
Jason Clarke <jclarke@salmon.com>
2017-11-02 11:40:47 +0000

    have buildCarousel Example

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+new Carousel(
+{
+  containerSel: "#dog",
+  renderFromJSHTMLTemplate: true,
+  images: ["http://media.timeout.com/blogimages/wp-content/uploads/2013/01/tumblr_lw7h7q5JKc1r3pgwbo1_500.jpeg",
+  "https://metrouk2.files.wordpress.com/2017/01/512513451.jpg?quality=80&strip=all",
+  "https://metrouk2.files.wordpress.com/2016/04/11951840_382970415232674_1167170684506797359_n.jpg?quality=80&strip=all"
+],
+SVGInit: true
+}
+)
+
 //Create the default carousel
 new Carousel(
   {
@@ -14,37 +26,26 @@ new Carousel(
   }
 )
 
-// new Carousel(
-// {
-//   containerSel: "#dog",
-//   renderFromJSHTMLTemplate: true,
-//   images: ["http://media.timeout.com/blogimages/wp-content/uploads/2013/01/tumblr_lw7h7q5JKc1r3pgwbo1_500.jpeg",
-//   "https://metrouk2.files.wordpress.com/2017/01/512513451.jpg?quality=80&strip=all",
-//   "https://metrouk2.files.wordpress.com/2016/04/11951840_382970415232674_1167170684506797359_n.jpg?quality=80&strip=all"
-// ],
-// paginationInit: false,
-// buttonInit: false
-// }
-// )
+
 
 //create a non-default carousel
-new Carousel(
-  {
-    containerSel: "#curlyCarousel",
-    initPicIndex: 2,
-    SVGInit: true,
-    frame: "square",
-    width: 50,
-    pagination: {
-      entranceAnim: "anim-select-top"
-    },
-    customListeners: [{
-        className: "magicButton",
-        eventType: "click",
-        entranceAnim: "anim-select-bigShrink",
-        exitAnim: "anim-deselect-shrink",
-        newPicIdFn: 'goRandom'
-    }
-  ]
-  }
-);
+// new Carousel(
+//   {
+//     containerSel: "#curlyCarousel",
+//     initPicIndex: 2,
+//     SVGInit: true,
+//     frame: "square",
+//     width: 50,
+//     pagination: {
+//       entranceAnim: "anim-select-top"
+//     },
+//     customListeners: [{
+//         className: "magicButton",
+//         eventType: "click",
+//         entranceAnim: "anim-select-bigShrink",
+//         exitAnim: "anim-deselect-shrink",
+//         newPicIdFn: 'goRandom'
+//     }
+//   ]
+//   }
+// );
